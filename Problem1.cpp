a391ea34e4454a318890ff09f35067d4f7388f9c
oussama-essadek <OUSSAMAESSADEK441@GMAIL.COM>
2026-09-12 22:35:02 +0100

    Add PrintName function to display a name

---
+++ Problem1.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+
+void PrintName(string Name) {
+	cout << "\nYour Name is : " << Name << endl;
+
+}
+int main() {
+    PrintName("oussama");
+	return 0;
+}
